525dec48c671b180eff29c8e891134d685a60bae
Abdelrahmam Salem <abubakir1997@gmail.com>
2020-01-05 15:57:30 -0500

    Fixed isPureFunction

--- src/isPureFunction.js
+++ src/isPureFunction.js
@@ -1,6 +1,6 @@
 export default function isPureFunction(func) {
     return (
-        typeof func === 'function' &&
+        typeof func === 'function' ||
         func.__proto__.constructor.toString() === 'function Function() { [native code] }'
     )
 }
